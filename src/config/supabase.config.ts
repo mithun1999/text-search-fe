6cd8be1aef4980765eddd747f9fc162a680e2fdd
Mithun <mithun.dev51@gmail.com>
2023-07-26 17:25:14 +0530

    refactor: remove unused packages

--- src/config/supabase.config.ts
+++
@@ -1,8 +0,0 @@
-export const supabaseConfig = {
-  supabaseUrl:
-    (import.meta.env.VITE_SUPABASE_URL as string) ||
-    "https://wiiffqrxlylhjwtznymy.supabase.co",
-  supabaseKey:
-    (import.meta.env.VITE_SUPABASE_KEY as string) ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndpaWZmcXJ4bHlsaGp3dHpueW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg2OTMxODEsImV4cCI6MTk5NDI2OTE4MX0.gorbYeKGlKL3cYj21uSPGVXKInDt_u7vI-doPZU2SBY",
-};
